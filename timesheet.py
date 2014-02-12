8788a574b50e455853a0070fadd0e7eeb5809b4c
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-12 14:42:20 +0200

    Adding task to timesheet view

--- timesheet.py
+++ timesheet.py
@@ -13,13 +13,19 @@ class analytic_timesheet_task(osv.osv):
     def write(self, cr, uid, ids, vals, context=None):
         #Your code goes here
         logger.log(logging.INFO, "saving task work")
-        print str(vals)
+        print str(ids)
+        if hasattr(vals, 'task'):
+            taskObj = self.pool.get('project.task')
+            task = taskObj.browse(cr, uid, vals['task'], context)
+            print 'task work lines: ' + str(task.work_ids)
         res = super(analytic_timesheet_task, self).write(cr, uid, ids, vals, context=context)
         return res
         
     _columns={
+        'task_work_line_id': fields.integer('Task work line in the selected task')
         'project_id': fields.many2one('project.project', 'Project'),
         'task': fields.many2one('project.task', 'Project Task'),
     }
+    
     
 analytic_timesheet_task()
