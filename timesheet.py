5706a0e977a4e7c5a05f3123d6337b6ef164f510
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-12 14:09:33 +0200

    Adding task to timesheet view

--- timesheet.py
+++ timesheet.py
@@ -1,32 +1,9 @@
 from osv import fields, osv
+from openerp.tools.translate import _
 
-'''
-class timesheet_task(osv.osv):
-    _name='hr_timesheet_sheet.sheet'
-    _inherit='hr_timesheet_sheet.sheet'
-    
-    def write(self, cr, uid, ids, vals, context=None):
-        #Your code goes here
-        res = super(timesheet_task, self).write(cr, uid, ids, vals, context=context)
-        return res
-        
-    _columns={
-        'task_id': fields.many2one('project.task', 'Project Task')#fields.related('line_id', 'workcenter', type='many2one', relation="project.task", string="Task"),
-    }
+import logging
 
-timesheet_task()
-'''
-'''
-class analytic_line(osv.osv):
-    _name="account.analytic.line"
-    _inherit="account.analytic.line"
-    
-    _columns={
-        'task': fields.many2one('project.task', 'Project Task'),
-    }
-    
-analytic_line()
-'''
+logger = logging.getLogger(__name__)
 
 class analytic_timesheet_task(osv.osv):
     _name="hr.analytic.timesheet"
@@ -35,11 +12,13 @@ class analytic_timesheet_task(osv.osv):
     
     def write(self, cr, uid, ids, vals, context=None):
         #Your code goes here
-        
+        logger.log(logging.INFO, "saving task work")
+        print str(vals)
         res = super(analytic_timesheet_task, self).write(cr, uid, ids, vals, context=context)
         return res
         
     _columns={
+        'project_id': fields.many2one('project.project', 'Project'),
         'task': fields.many2one('project.task', 'Project Task'),
     }
     
