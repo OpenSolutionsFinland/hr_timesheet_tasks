9f4bcbacc878f6cc2dca81e02616e931b9a9dd46
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-12 13:20:26 +0200

    Adding task to timesheet view

---
+++ timesheet.py
@@ -0,0 +1,48 @@
+from osv import fields, osv
+
+
+class timesheet_task(osv.osv):
+    _name='hr_timesheet_sheet.sheet'
+    _inherit='hr_timesheet_sheet.sheet'
+    
+    def write(self, cr, uid, ids, vals, context=None):
+        #Your code goes here
+        res = super(timesheet_task, self).write(cr, uid, ids, vals, context=context)
+        return res
+        
+    _columns={
+        'task': fields.many2one('project.task', 'Project Task')#fields.related('line_id', 'workcenter', type='many2one', relation="project.task", string="Task"),
+    }
+
+timesheet_task()
+
+
+'''
+class analytic_line(osv.osv):
+    _name="account.analytic.line"
+    _inherit="account.analytic.line"
+    
+    _columns={
+        'workcenter': fields.many2one('mrp.routing.workcenter', 'Work type'),
+    }
+    
+analytic_line()
+'''
+'''
+class analytic_timesheet_task(osv.osv):
+    _name="hr.analytic.timesheet"
+    _inherit="hr.analytic.timesheet"
+    
+    
+    def write(self, cr, uid, ids, vals, context=None):
+        #Your code goes here
+        
+        res = super(analytic_timesheet_task, self).write(cr, uid, ids, vals, context=context)
+        return res
+        
+    _columns={
+        'workcenter_id': fields.related('line_id', 'workcenter', type='many2one', relation="project.task", string="Task"),
+    }
+    
+analytic_timesheet_task()
+'''
