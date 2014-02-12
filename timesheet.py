d9f5f8d5e3b2b47a2c2bdee3dfa6de4717a14d8d
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-12 15:35:28 +0200

    Implementing task work creation / deletion

--- timesheet.py
+++ timesheet.py
@@ -14,10 +14,48 @@ class analytic_timesheet_task(osv.osv):
         #Your code goes here
         logger.log(logging.INFO, "saving task work")
         print str(ids)
-        if hasattr(vals, 'task'):
+        print str(vals)
+        currentWorkLineID = 0
+        try:
+            lineObj = self.pool.get('hr.analytic.timesheet')
+            line = lineObj.browse(cr, uid, ids, context)[0]
+            currentWorkLineID = line.task_work_line_id
+            
             taskObj = self.pool.get('project.task')
             task = taskObj.browse(cr, uid, vals['task'], context)
-            print 'task work lines: ' + str(task.work_ids)
+            taskWorkObj = self.pool.get('project.task')
+            
+            description =  vals['name'] if hasattr(vals, 'name') else line.name
+            date = vals['date'] if hasattr(vals, 'date') else line.date
+            unit_amount = vals['unit_amount'] if hasattr(vals, 'unit_amount') else line.unit_amount
+            project = vals['project_id'] if hasattr(vals, 'project_id') else line.project_id.id
+            task = vals['task'] if hasattr(vals, 'task') else: line.task.id
+            
+            workVals = {
+                'name': description,
+                'project_id': project,
+                'task_id': task,
+                'date': date,
+                'hours': unit_amount,
+            }
+            
+            # if project or task has changed remove old work from previous task
+            if hasattr(vals, 'project_id') or hasattr(vals, 'task'):
+                logger.log(logging.INFO, "project or task changed")
+                # remove old task work
+                if currentWorkLineID != 0:
+                    taskWorkObj.unlink(cr, uid, currentWorkLineID, context)
+                
+                currentWorkLineID = taskWorkObj.create(cr, uid, workVals, context)
+            else:
+                # write to old task work
+
+                taskWorkObj.write(cr, uid, currentWorkLineID, workVals, context)
+        except e:
+            logger.log(logging.ERROR, e)
+            pass
+        
+        vals['task_work_line_id'] = currentWorkLineID
         res = super(analytic_timesheet_task, self).write(cr, uid, ids, vals, context=context)
         return res
         
@@ -27,5 +65,8 @@ class analytic_timesheet_task(osv.osv):
         'task': fields.many2one('project.task', 'Project Task'),
     }
     
+    _defaults = {
+        'task_work_line_id': 0,
+    }
     
 analytic_timesheet_task()
