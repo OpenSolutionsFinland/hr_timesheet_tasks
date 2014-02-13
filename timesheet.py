5f07110544767aa5c3482752964c931df9706bc6
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-13 14:43:27 +0200

    implementing create and delete for task work

--- timesheet.py
+++ timesheet.py
@@ -14,9 +14,9 @@ class analytic_timesheet_task(osv.osv):
         res = super(analytic_timesheet_task, self).unlink(cr, uid, ids, vals, context=context)
         return res
         
-    def create(self, cr, uid, ids, vals, context=None):
+    def create(self, cr, uid, vals, context=None):
         logger.log(logging.INFO, "creating task work")
-        res = super(analytic_timesheet_task, self).create(cr, uid, ids, vals, context=context)
+        res = super(analytic_timesheet_task, self).create(cr, uid, vals, context=context)
         return res
         
     def write(self, cr, uid, ids, vals, context=None):
